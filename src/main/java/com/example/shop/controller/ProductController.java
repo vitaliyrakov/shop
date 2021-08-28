7ca9004614979b08c0ec8704163830ba26e956ae
VitaliyR <vitaliy.rakov@gmail.com>
2021-08-28 20:39:29 +0300

    hw6

--- src/main/java/com/example/shop/controller/ProductController.java
+++ src/main/java/com/example/shop/controller/ProductController.java
@@ -12,7 +12,7 @@ import javax.annotation.security.RolesAllowed;
 @Controller
 @RequestMapping("/products")
 @RequiredArgsConstructor
-//@RolesAllowed("ADMIN")
+@RolesAllowed("ADMIN")
 public class ProductController {
 
     private final ProductService productService;
